a406680445b4ec4f280773562591ac4ae3a28470
unknown <praveensn0088@gmail.com>
2025-07-06 13:21:43 +0530

    modular code changes

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retreived context to answer "
+    "the questsion. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    " answer concise. "
+    "\n\n"
+    "{context}"
+)
