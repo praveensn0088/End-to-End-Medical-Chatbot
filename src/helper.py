d14c4d251ea11d17f06541c4982040ead7c7c6e2
unknown <praveensn0088@gmail.com>
2025-07-07 07:48:06 +0530

    original key removed

--- src/helper.py
+++ src/helper.py
@@ -14,10 +14,6 @@ def text_split(extracted_data):
     text_chunks = text_splitter.split_documents(extracted_data)
     return text_chunks
 
-def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size =500, chunk_overlap = 20) 
-    text_chunks = text_splitter.split_documents(extracted_data)
-    return text_chunks
 
 def download_hugging_face_embeddings(): 
     embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
